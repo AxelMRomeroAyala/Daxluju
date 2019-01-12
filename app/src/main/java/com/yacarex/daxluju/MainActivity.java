76f1921feba9f61b8b1231054a053531b5ab2d57
AxelCarbonX1 <axel@iamat.com>
2019-01-12 18:49:47 -0300

    Added 4 buttons

--- app/src/main/java/com/yacarex/daxluju/MainActivity.java
+++ app/src/main/java/com/yacarex/daxluju/MainActivity.java
@@ -1,13 +1,34 @@
 package com.yacarex.daxluju;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button daniButton, axelButton, luchoButton, juanfraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpButtons();
+    }
+
+    private void setUpButtons(){
+        daniButton= findViewById(R.id.dani_button);
+        axelButton= findViewById(R.id.axel_button);
+        luchoButton= findViewById(R.id.lucho_button);
+        juanfraButton= findViewById(R.id.juanfra_button);
+
+        axelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), AxelActivity.class));
+            }
+        });
     }
 }
