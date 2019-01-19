11bf6eb781a067ec747e392456a8aa7ef316e976
AxelCarbonX1 <axel@iamat.com>
2019-01-19 20:21:16 -0300

    Added a button that loads a random image from internet.

--- app/src/main/java/com/yacarex/daxluju/AxelActivity.java
+++ app/src/main/java/com/yacarex/daxluju/AxelActivity.java
@@ -4,15 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 
 public class AxelActivity extends AppCompatActivity {
 
     TextView axelTextView;
-    Button editar;
+    ImageView axelImage;
+    Button editar, editar2;
 
-    String edicion= "EDITADO";
-    String edicion2= "EDITADO 2";
+    String edicion = "EDITADO";
+    String edicion2 = "EDITADO 2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +26,23 @@ public class AxelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_axel);
 
         axelTextView = findViewById(R.id.axel_text);
-        editar= findViewById(R.id.axel_editar);
+        editar = findViewById(R.id.axel_editar);
+        editar2 = findViewById(R.id.axel_editar_2);
+        axelImage = findViewById(R.id.axel_image);
 
         editar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 axelTextView.setText(edicion);
+            }
+        });
+        editar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                Glide.with(getBaseContext())
+                        .load("https://picsum.photos/200/300/?random")
+                        .into(axelImage);
             }
         });
     }
