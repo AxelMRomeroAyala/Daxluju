1f8af907a01771590cf2f337610056760f49c8ac
AxelMaximilianoRomeroAyala <axel@iamat.com>
2019-01-19 18:59:46 -0300

    Toy trabajando vieja

--- app/src/main/java/com/yacarex/daxluju/AldeanActivity.java
+++ app/src/main/java/com/yacarex/daxluju/AldeanActivity.java
@@ -2,8 +2,14 @@ package com.yacarex.daxluju;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AldeanActivity extends AppCompatActivity {
+
+    private TextView door;
+    private Button openUP;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
