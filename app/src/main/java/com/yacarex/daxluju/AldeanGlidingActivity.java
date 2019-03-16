9955feecfda7d05863254c08ed1368de9a65d566
Lucho <lucianop.pereira20@gmail.com>
2019-03-16 17:33:22 -0300

    GLIDE CORREGIDO

--- app/src/main/java/com/yacarex/daxluju/AldeanGlidingActivity.java
+++ app/src/main/java/com/yacarex/daxluju/AldeanGlidingActivity.java
@@ -1,7 +1,7 @@
 package com.yacarex.daxluju;
 
-import android.support.constraint.solver.Cache;
-import android.support.v4.widget.CircularProgressDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,15 +10,19 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.signature.ObjectKey;
 
 public class AldeanGlidingActivity extends AppCompatActivity {
 
     private ImageView glideView;
     private Button loadButton, cleanButton;
-//    private ProgressBar progressBar;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +32,51 @@ public class AldeanGlidingActivity extends AppCompatActivity {
         glideView   = findViewById(R.id.glideImageView);
         loadButton  = findViewById(R.id.imageLoadButton);
         cleanButton = findViewById(R.id.imageCleanButton);
+        progressBar = findViewById(R.id.progressBar);
+
+        ;
 
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                CircularProgressDrawable progressBar = new CircularProgressDrawable(getBaseContext());
-                progressBar.setStrokeWidth(10f);
-                progressBar.setCenterRadius(100f);
-                progressBar.setBackgroundColor(5);
-                progressBar.start();
+                progressBar.setVisibility(View.VISIBLE);
 
                 Glide.with(getBaseContext())
-                        .load("https://oyster.ignimgs.com/mediawiki/apis.ign.com/spyro-the-dragon/b/b3/Spyro_Reignited_Trilogy_20181115213349.png")
-                        .apply(new RequestOptions()
-                                .placeholder(progressBar)
-                                .signature(new ObjectKey("aldeanGlideCache"))
-                                .skipMemoryCache(true))
-                        .transition(DrawableTransitionOptions.withCrossFade(200))
-                        .into(glideView);
+                    .load("https://oyster.ignimgs.com/mediawiki/apis.ign.com/spyro-the-dragon/b/b3/Spyro_Reignited_Trilogy_20181115213349.png")
 
-            }
+                    .apply(new RequestOptions()
+                        .signature(new ObjectKey("aldeanGlideCache"))
+                        .skipMemoryCache(true))
+                    .transition(DrawableTransitionOptions.withCrossFade(200))
+                    .addListener(new RequestListener<Drawable>() {
+
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+
+                            progressBar.setVisibility(View.GONE);
+                            glideView.setImageResource(R.drawable.fbi_logo);
+                            return false;
+
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+
+                            progressBar.setVisibility(View.GONE);
+                            return false;
+
+                        }
+                    })
+                    .into(glideView);
+
+                }
         });
 
         cleanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
                 glideView.setImageResource(0);
 
